415d78539429ae95a12e9fdfc0ea04ed594a6c03
Miguel <migueljm1008@gmail.com>
2025-07-16 22:13:12 +0200

    add/filtrador+validacion+pintado de mapa2

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const popup = L.popup()
     .setContent("El parque mas bonito")
     .openOn(map);
 
-/***************** Ejercicio terremotos mapa 1 **************/
+/***************** Ejercicio terremotos mapa 1 fetch data **************/
 async function getData(){
     try{
         const res= await fetch("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson");
@@ -56,14 +56,14 @@ async function getData(){
 /****************** Pintar mapa 1 ****************/
 getData().then(data => {
     
-    console.log(data);
+    //console.log(data);
     //Agregar marcador
 
     data.map(pin => {
         //Tratamiento de datos
         // [lat,lon]
         const coordinates_pin = [pin.geometry.coordinates[1],pin.geometry.coordinates[0]];
-        console.log(coordinates_pin);
+        //console.log(coordinates_pin);
         const fechaTerremoto = new Date(pin.properties.time);
 
         // Representación de datos
@@ -80,18 +80,90 @@ getData().then(data => {
         .addTo(map);
     })
 });
-/******************** Validacion filtro ******************/
-/***************** Terremotos mapa 2 ****************/
-// async function getFilteredData(){
-//     try{
-//         const res= await fetch("");
 
-//         const data = await res.json();
-//         return data.features;
-//     }
-//     catch (error) {
-//     console.error('Hubo un problema con la solicitud:', error.message);
-//     }
-// }
+/************ Crea mapa 2 ****************/
+var map2 = L.map('map2').setView([40.408072, -3.676729], 5);
+const marcadoresDeTerremotos = L.layerGroup().addTo(map2);
+
+/*************** Añadir estilo al mapa *************/
+var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 0,
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'png'
+}).addTo(map2);
+
+/***************** Terremotos mapa 2 fetch data filtrada ****************/
+async function getFilteredData(magnitud, startDate, endDate){
+    try{
+        const res= await fetch(`https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=${startDate}&endtime=${endDate}&minmagnitude=${magnitud}`);
+
+        const data = await res.json();
+        console.log(data.features);
+        return data.features;
+    }
+    catch (error) {
+    console.error('Hubo un problema con la solicitud:', error.message);
+    }
+}
+/********************* Filtro combinado *********************/
+document.getElementById('filter').addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const magnitud = event.target.elements['mag-filter'].value;
+    const startDate = event.target.elements.startDate.value.trim();
+    const endDate = event.target.elements.endDate.value.trim();
+
+/******************** Validacion filtro ******************/
+    let msj = '';
+
+    if (startDate > endDate || endDate < startDate){
+        console.log('Fechas no validas');
+        msj += 'Fechas no validas\n';
+    }
+
+    if (magnitud > 7 || magnitud < 0) {
+        console.log('Magnitud no valida');
+        msj += 'Magnitud no valida\n';
+    }
+
+    if (msj.length !=0) {
+    alert(msj);
+    let p = document.createElement('pre');
+    let message = document.createTextNode(msj);
+    p.style.color = '#DD1C1A';
+    p.style.fontSize = '16px';
+    p.appendChild(message);
+
+    document.getElementById('filter').appendChild(p)
+}
 /***************** Pintar mapa 2 *****************/
+    else {
+        getFilteredData(magnitud, startDate, endDate)
+        .then(data => {
+            marcadoresDeTerremotos.clearLayers();
+            data.map(pin => {
+
+                const coordinates_pin = [pin.geometry.coordinates[1],pin.geometry.coordinates[0]];
+      
+                const fechaTerremoto = new Date(pin.properties.time);
+
+                var colorMarker = L.AwesomeMarkers.icon({
+                markerColor: pintarColores(pin.properties.mag)
+                });
+
+
+                const marker = L.marker(coordinates_pin, {icon: colorMarker})
+                .bindPopup(`Titulo:${pin.properties.title}<br>
+                    Fecha:${fechaTerremoto}<br>
+                    Ubicación:${pin.properties.place}<br>
+                    Código:${pin.properties.code}
+                    Magnitud:${pin.properties.mag}`)
+                .addTo(marcadoresDeTerremotos);    
+            })
+        })
+    }
+});
+
+
 // Borrar marcadores: clearLayers()
